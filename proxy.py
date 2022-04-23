a173c125b18b01c14c1dc37ece5b55bebdd0092f
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2022-04-23 21:42:18 +0700

    update

--- proxy.py
+++ proxy.py
@@ -18,19 +18,19 @@ while True:
 
     print(temp, humid, response)
 
-    f1 = requests.post(
-        "https://io.adafruit.com/api/v2/khanhad69/feeds/iot-dadn.fan-status-1/data", headers={
-            'X-AIO-Key': 'aio_NmDN7114NKG08q56uoHiKtBNLwWy'
-        }, json={'value': 1}).json()
+    # f1 = requests.post(
+    #     "https://io.adafruit.com/api/v2/khanhad69/feeds/iot-dadn.fan-status-1/data", headers={
+    #         'X-AIO-Key': 'aio_NmDN7114NKG08q56uoHiKtBNLwWy'
+    #     }, json={'value': 1}).json()
 
-    f2 = requests.post(
-        "https://io.adafruit.com/api/v2/khanhad69/feeds/iot-dadn.fan-status-2/data", headers={
-            'X-AIO-Key': 'aio_NmDN7114NKG08q56uoHiKtBNLwWy'
-        }, json={'value': 1}).json(),
+    # f2 = requests.post(
+    #     "https://io.adafruit.com/api/v2/khanhad69/feeds/iot-dadn.fan-status-2/data", headers={
+    #         'X-AIO-Key': 'aio_NmDN7114NKG08q56uoHiKtBNLwWy'
+    #     }, json={'value': 1}).json(),
 
-    f3 = requests.post(
-        "https://io.adafruit.com/api/v2/khanhad69/feeds/iot-dadn.fan-status-3/data", headers={
-            'X-AIO-Key': 'aio_NmDN7114NKG08q56uoHiKtBNLwWy'
-        }, json={'value': 1}).json()
+    # f3 = requests.post(
+    #     "https://io.adafruit.com/api/v2/khanhad69/feeds/iot-dadn.fan-status-3/data", headers={
+    #         'X-AIO-Key': 'aio_NmDN7114NKG08q56uoHiKtBNLwWy'
+    #     }, json={'value': 1}).json()
 
     print(f1, f2, f3)
